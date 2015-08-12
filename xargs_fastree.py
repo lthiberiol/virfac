5a5a4a6b9171396db4d7574793d7bf07f5250feb
Thiberio Rangel <lthiberiol@gmail.com>
2015-08-12 14:00:12 -0400

    changes to use nucleotide sequences

--- xargs_fastree.py
+++ xargs_fastree.py
@@ -3,6 +3,6 @@
 from os import system
 from sys import argv
 
-out_folder = '../trees'
+out_folder = '../fasttree'
 
-system('fasttree %s > %s/%s' %(argv[1], out_folder, argv[1].replace('.aln', '.tre')))
+system('fasttree -nt -gtr -gamma %s > %s/%s' %(argv[1], out_folder, argv[1].replace('.aln', '.tre')))
