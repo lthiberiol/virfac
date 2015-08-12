95f490c76bfd6cf269095d471746bd404dcd6d12
Thiberio Rangel <lthiberiol@gmail.com>
2015-08-12 13:57:55 -0400

    changes to generate fastas of only groups in at least 10 genomes, and using nucleotide sequences

--- creates_fastas_from_sql.py
+++ creates_fastas_from_sql.py
@@ -1,5 +1,3 @@
-__author__ = 'Thiberio'
-
 from pickle import load
 import MySQLdb as sql
 
@@ -9,14 +7,20 @@ cursor = db.cursor()
 t3ss_groups = load(open('t3ss_groups-merged.pkl'))
 groups = load(open('homologous_groups-merged.pkl'))
 
-for group in t3ss_groups:
+for group in groups:
+    if len(groups[group]) < 10:
+        continue
+
     sql_query = []
     for genome in groups[group]:
         for protein in groups[group][genome]:
             sql_query.append('(genome="%s" and seq_id="%s")' %(genome, protein))
-    cursor.execute('select seq_id, genome, aminoacid from sequences where %s' %' or '.join(sql_query))
 
-    out = open('interproscan-merged/fastas/%s.faa' %group.replace('&', '-'), 'wb')
+    cursor.execute('select seq_id, genome, nucleotide from sequences_all where %s' %' or '.join(sql_query))
+
+    out = open('tree_puzzle/nucleotide/fastas/%s.fna' %group.replace('&', '-'), 'wb')
     for query_result in cursor.fetchall():
         out.write('>%s|%s\n%s\n' %query_result)
     out.close()
+
+    break
