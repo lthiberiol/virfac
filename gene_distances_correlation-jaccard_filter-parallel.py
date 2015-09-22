c88524fc5334fe765b8c545ecac7a3bd8ac4ec15
Thiberio Rangel <lthiberiol@gmail.com>
2015-09-22 16:56:17 -0400

    lot of weird changes!

--- gene_distances_correlation-jaccard_filter-parallel.py
+++ gene_distances_correlation-jaccard_filter-parallel.py
@@ -14,11 +14,13 @@ import multiprocessing
 import networkx as nx
 import numpy as np
 from statsmodels.sandbox.stats.multicomp import fdrcorrection0 as fdr
+from skbio.stats.distance import mantel, DistanceMatrix as dm
 
-main_dir = ('/Volumes/Macintosh HD 2/thiberio/virulence_factors/')
+main_dir = '/Volumes/Macintosh HD 2/thiberio/virulence_factors/'
 
 groups = load( open( '%s/homologous_groups-merged.pkl' %main_dir ) )
 pb = pd.read_table('%s/presence_absence-merged.tab' %main_dir, index_col=0, nrows=105)
+print 'yeah'
 
 ######################################################
 ########################### finally, assess distances!
@@ -79,6 +81,89 @@ all_species_distances.to_csv('all_species_distances.tab', sep='\t')
 ######################################################
 ######################## finally, assess correlations!
 ######################################################
+ignored_species = ['A_schubertii_CECT4240T', 'A_diversa_CECT4254T', 'A_simiae_CIP107798T']
+
+dist_matrices = []
+group_ids     = []
+for n in listdir('distances'):
+    if not n.endswith('.phy.dist.tab'):
+        continue
+
+    group_name = n.replace('.phy.dist.tab', '')
+    tmp = pd.read_table('distances/%s' %n, index_col=0)
+    taxa = []
+    for taxon in tmp.index:
+        taxa.append(taxon.split('|')[1])
+
+    if len(taxa) > len( set( taxa ) ):
+        continue
+
+    tmp.index   = taxa
+    tmp.columns = taxa
+
+    should_be_dropped = list(np.intersect1d(taxa, ignored_species))
+
+    if should_be_dropped:
+        tmp.drop(should_be_dropped, axis=0, inplace=True)
+        tmp.drop(should_be_dropped, axis=1, inplace=True)
+
+    dist_matrices.append(  dm(tmp.as_matrix(), tmp.index) )
+    group_ids.append(                          group_name )
+
+
+def assess_corr(pairs):
+    condensed_corr = []
+    condensed_pval = []
+    
+    for pair in pairs:
+
+        j_index =  len( np.intersect1d( pair[0].ids, pair[1].ids ) ) / float( len( np.union1d( pair[0].ids, pair[1].ids ) ) ) 
+        if j_index < 0.8:
+            (rho, p) = (np.nan, np.nan)
+        else:
+            (rho, p, n) = mantel(pair[0], pair[1], strict=False)
+
+        condensed_corr.append(rho)
+        condensed_pval.append(p  )
+
+    return (condensed_corr, condensed_pval)
+
+group_pairs = list(combinations(dist_matrices, 2))
+num_of_threads = 15
+num_of_comparisons = len(group_pairs)
+print "\t** Breaking datasets..."
+avg = num_of_comparisons / float(num_of_threads)
+datasets = []
+last = 0.0
+while last < len(group_pairs):
+    datasets.append(group_pairs[int(last):int(last + avg)])
+    last += avg
+print "\t... done!\n"
+
+print "\t** Getting shit done..."
+start_time = time()
+pool = multiprocessing.Pool(processes=num_of_threads)
+results = pool.map(assess_corr, datasets)
+pool.close()
+pool.join()
+print "\t... done!\n"
+print time() - start_time
+
+print "\t... almost, just organizing ..."
+
+condensed_corr = []
+condensed_pval = []
+for thread_result in results:
+    condensed_corr.extend(thread_result[0])
+    condensed_pval.extend(thread_result[1])
+condensed_corr = np.array(condensed_corr)
+condensed_pval = np.array(condensed_pval)
+print "\t... done!\n"
+print time() - start_time
+
+
+
+
 
 #
 # if want to read a pre-computed distance DataFrame, uncomment line below
@@ -173,15 +258,234 @@ print "\t**Organizing condensed matrices into something you can understand (you 
 uncorrected_corr_matrix = squareform(condensed_corr)
 for n in range(uncorrected_corr_matrix.shape[0]):
     uncorrected_corr_matrix[n][n] = 1.
-uncorrected_corr_df = pd.DataFrame(index=all_species_distances.index, columns=all_species_distances.index, data=uncorrected_corr_matrix)
+uncorrected_corr_df = pd.DataFrame(index=group_ids, columns=group_ids, data=uncorrected_corr_matrix)
 
-rejecteds_df = pd.DataFrame(index=all_species_distances.index, columns=all_species_distances.index, data=squareform(should_reject_rho) > 0)
+rejecteds_df = pd.DataFrame(index=group_ids, columns=group_ids, data=squareform(should_reject_rho) > 0)
 corr_df = uncorrected_corr_df[rejecteds_df]
-corr_df.to_csv('significant_gene_families_dist_correlations.tab', sep='\t')
+corr_df.to_csv('significant_gene_families_mantel_dist_correlations.tab', sep='\t')
 print "\t... done!\n"
 
 #
 # network time!
+#
+corr_df = pd.read_table('significant_gene_families_mantel_dist_correlations.tab', index_col=0)
+graph = nx.Graph()
+
+indexes = corr_df.index
+graph.add_nodes_from(corr_df.index)
+
+print '\t**Adding edges ...'
+edges_between = list( combinations( indexes, 2 ) )
+weights       = squareform( corr_df.fillna(0).values )
+threshold = 0.7
+for n in range( len( weights) ):
+    edge_weight = weights[n]
+    if edge_weight >= threshold:
+        connected_nodes = edges_between[n]
+        graph.add_edge( connected_nodes[0], connected_nodes[1], weight = edge_weight )
+
+degrees = graph.degree()
+nodes_with_no_connections = []
+for node in degrees.iteritems():
+    if node[1] == 0 or node[0] not in groups:
+        nodes_with_no_connections.append(node[0])
+graph.remove_nodes_from(nodes_with_no_connections)
+
+group_length = []
+node_sizes   = []
+node_colors  = []
+for node in graph.nodes():
+    node_sizes.append( len( groups[node] ) )
+    node_colors.append( degrees[node] )
+
+#    phylip_header = open('phylip/%s.phy' %node.replace('&', '-')).xreadlines().next()
+#    group_length.append( int( phylip_header.split()[1] ) )
+print '... done!'
+
+print '\t**Generating spring layout ...'
+graph_layout = nx.spring_layout(graph)#, iterations=500)
+print '... done!'
+
+plt.figure()
+plt.xticks([])
+plt.yticks([])
+
+print '\t**Drawing nodes ...'
+nx.draw_networkx_nodes(graph, pos=graph_layout, node_size=node_sizes, node_color=node_colors, with_labels=False, alpha=0.75, cmap=plt.get_cmap('Blues'))
+print '\t**Saving pdf (no edges) ...'
+plt.savefig('mantel_correlation_network-no_cutoff-ignored_species-no_edges.pdf', bbox_inches='tight')#, dpi=100, figsize=(15,15))
+print '\t**Drawing edges ...'
+nx.draw_networkx_edges(graph, edgelist=edgelist, pos=graph_layout, with_labels=False, alpha=0.3)
+print '\t**Saving final pdf ...'
+plt.savefig('mantel_correlation_network-no_cutoff-ignored_species.pdf', bbox_inches='tight')#, dpi=100, figsize=(15,15))
+print '... done!'
+plt.close()
+
+######################################################
+############################################ MCL time!
+######################################################
+print '\t**Saving network matrix ...'
+nx.write_weighted_edgelist(graph, 'mcl_mantel_input-%.1f.abc' %threshold)
+
+print '\t**Writing in MCL-happy format ...'
+system('mcxload -abc mcl_mantel_input-%.1f.abc --stream-mirror -o mcl_mantel_input-%.1f.mci -write-tab mcl_mantel_input-%.1f.tab' %(threshold, threshold, threshold))
+
+print '\t**Running MCL with different inflation values (-I) ...'
+system('cat inflation_values | xargs -n1 -P10 mcl mcl_mantel_input-%.1f.mci -I ' %threshold)
+system('clm dist --chain out.mcl_mantel_input-%.1f.mci.I{14,20,25,30,35,40,45,50,55,60,65,70} > clm_dist-%.1f' %threshold)
+
+print '\t**Running MCL with selected inflation ...'
+system('mcl mcl_mantel_input-%.1f.mci -I 4.5 -te 10 -o mcl_mantel_output-%.1f' %(threshold, threshold))
+
+system('mcxdump -icl mcl_mantel_output-%.1f -tabr mcl_mantel_input-%.1f.tab -o gene_clusters-I45-mantel-%.1f' %(threshold, threshold, threshold))
+print '... done!'
+
+gene_clusters = []
+for line in open('test/gene_clusters-0.7').xreadlines():
+    gene_clusters.append(line.split())
+top20 = []
+for cluster in gene_clusters[:20]:
+    top20.extend(cluster)
+top40 = []
+for cluster in gene_clusters[:40]:
+    top40.extend(cluster)
+
+weights = {}
+nodes = []
+for cluster in range(10):
+    nodes.extend(gene_clusters[cluster])
+for edge in graph.subgraph(nodes).edges(data=True):
+    (a,b) = (False, False)
+    for cluster in range(10):
+        if edge[0] in gene_clusters[cluster]:
+            a = cluster
+        if edge[1] in gene_clusters[cluster]:
+            b = cluster
+        if a and b:
+            break
+
+    keys = [a,b]
+    keys.sort()
+    key = str(keys[0])+','+str(keys[1])
+    if key not in weights:
+        weights[key] = []
+
+    weights[key].append(edge[2]['weight'])
+
+print 'yeah'
+
+for i in range(10):
+    i = str(i)
+    
+    for j in range(10):
+        j = str(j)
+
+        if i == j:
+            continue
+
+        if '%s,%s' %(i,j) in weights:
+            t,p = ttest_ind(weights['%s,%s' %(i,i)], weights['%s,%s' %(i,j)])
+            print '%s vs %s-> t: %.2f\tp:%.2e' %(i, j, t, p)
+
+tree_comp_dataset = {}
+for i in range(10):
+
+    for j in range(10):
+
+        pairs = []
+    
+        if i < j:
+            break
+        elif i == j:
+            pairs = list( combinations( gene_clusters[i],2 ) )
+        else:
+            pairs = list( product( gene_clusters[i], gene_clusters[j] ) )
+
+        print '%i vs %i' %(i,j)
+
+        if not pairs:
+            continue
+
+        shuffle(pairs)
+
+        key = '%i,%i' %(i, j)
+        if key not in tree_comp_dataset:
+            tree_comp_dataset[key] = []
+
+        for pair in pairs:
+            j_dist =  jaccard(pb[pair[0]], pb[pair[1]])
+
+            if j_dist > 0.2:
+                continue
+
+            tree_comp_dataset[key].append(pair)
+            if len(tree_comp_dataset[key]) == 100:
+                break
+
+        if len(tree_comp_dataset[key]) < 20:
+            print '\t%s is empty! =/' %key
+            tree_comp_dataset.pop(key)
+print 'yeah'
+
+def run_clann_rfdists(cluster_pair, tree_pairs):
+    tmp_input_file_name = 'tmp_input_rf_%f' %random.random()
+    tmp_output_file_name = 'tmp_output_rf_%f' %random.random()
+
+    for pair in tree_pairs:
+        system('cat fasttree-no_gene_id/%s.tre fasttree-no_gene_id/%s.tre > %s' %(pair[0], pair[1], tmp_input_file_name))
+        system('echo "execute %s; rfdists filename=%s; quit;" | clann' %(tmp_input_file_name, tmp_output_file_name))
+        system('cat %s >> %s.rf' %(tmp_output_file_name, cluster_pair))
+
+    system('rm %s' %tmp_input_file_name)
+    system('rm %s' %tmp_output_file_name)
+map(lambda x: run_clann_rfdists(x, tree_comp_dataset[x]), tree_comp_dataset.keys())
+
+def run_dp_wrfdist(tree_pairs):
+    dists = []
+
+    for pair in tree_pairs:
+        t1 = tree.get_from_path('fasttree-no_gene_id/%s.tre' %pair[0], 'newick', taxon_namespace=tns)
+        t2 = tree.get_from_path('fasttree-no_gene_id/%s.tre' %pair[1], 'newick', taxon_namespace=tns)
+        
+        t1.encode_bipartitions()
+        t2.encode_bipartitions()
+
+        dists.append( treecompare.weighted_robinson_foulds_distance( t1, t2 ) )
+
+    return dists
+yeah = {}
+yeah = map(lambda x: run_dp_wrfdist(tree_comp_dataset[x]), tree_comp_dataset.keys())
+
+rfdists = {}
+for cluster_pair in tree_comp_dataset:
+    rfdists[cluster_pair] = []
+    for dist in open('%s.rf' %cluster_pair).read().split():
+        rfdists[cluster_pair].append(float(dist))
+
+for pair in rfdists:
+    if pair[0] == pair[2]:
+        continue
+
+    t,p = ttest_ind(rfdists[pair], rfdists['%s,%s' %(pair[0],pair[0])])
+    print '%s vs %s,%s-> t: %.2f\tp:%.2e' %(pair, pair[0], pair[0], t, p)
+    t,p = ttest_ind(rfdists[pair], rfdists['%s,%s' %(pair[2],pair[2])])
+    print '%s vs %s,%s-> t: %.2f\tp:%.2e' %(pair, pair[2], pair[2], t, p)
+print 'yeah'
+
+
+
+
+        
+
+for cluster in gene_clusters:
+    for group in mls_groups:
+        if mls_groups[group] in cluster:
+            print group
+            print gene_clusters.index(cluster)
+            print ''
+
+#
+# network time, again!
 #
 graph = nx.Graph()
 
@@ -193,110 +497,67 @@ edges_between = list( combinations( indexes, 2 ) )
 weights       = squareform( corr_df.fillna(0).values )
 for n in range( len( weights) ):
     edge_weight = weights[n]
-    if edge_weight >= 0.9:
+    if edge_weight >= threshold:
         connected_nodes = edges_between[n]
         graph.add_edge( connected_nodes[0], connected_nodes[1], weight = edge_weight )
 
 degrees = graph.degree()
 nodes_with_no_connections = []
 for node in degrees.iteritems():
-    if node[1] == 0:
+    if node[1] == 0 or node[0] not in groups or node[0] not in top20:
         nodes_with_no_connections.append(node[0])
 graph.remove_nodes_from(nodes_with_no_connections)
 
-group_length = []
 node_sizes   = []
 node_colors  = []
 for node in graph.nodes():
     node_sizes.append( len( groups[node] ) )
-    node_colors.append( degrees[node] )
-
-    phylip_header = open('phylip/%s.phy' %node.replace('&', '-')).xreadlines().next()
-    group_length.append( int( phylip_header.split()[1] ) )
+    flag = True
+    for pos in range(20):
+        if node in gene_clusters[pos]:
+            node_colors.append(pos)
+            flag = False
+            break
+    if flag:
+        continue
+        node_colors.append(0)
 print '... done!'
 
 print '\t**Generating spring layout ...'
-graph_layout = nx.spring_layout(graph, iterations=500)
+graph_layout = nx.spring_layout(graph)
 print '... done!'
+
+cNorm  = colors.Normalize(vmin=0, vmax=41)
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=plt.get_cmap('Set1'))
 
 plt.figure()
 plt.xticks([])
 plt.yticks([])
 
 print '\t**Drawing nodes ...'
-nx.draw_networkx_nodes(graph, pos=graph_layout, node_size=node_sizes, node_color=node_colors, with_labels=False, alpha=0.75, cmap=plt.get_cmap('Blues'))
+nx.draw_networkx_nodes(graph, nodelist=not_top40, pos=graph_layout, node_size=node_sizes, node_color=['w']*len(not_top40), label='not top 40', with_labels=False, alpha=0.3)
+for cluster in range(40):
+    nx.draw_networkx_nodes(graph, nodelist= gene_clusters[cluster], pos=graph_layout, node_size=node_sizes, node_color=[scalarMap.to_rgba(cluster)] * len(gene_clusters[cluster]), label='cluster_%i' %cluster, with_labels=False, alpha=0.6)
+plt.legend(loc=9, scatterpoints=1, bbox_to_anchor=(0.5,0), ncol=9, fontsize='xx-small', columnspacing=0.3, handletextpad=0)
 print '\t**Saving pdf (no edges) ...'
-plt.savefig('correlation_network-ignored_species-no_edges.pdf', bbox_inches='tight')#, dpi=100, figsize=(15,15))
+plt.savefig('xmantel_correlation_network-uhuhuhtop40-I45-%.1f-ignored_species-no_edges10.pdf' %threshold, bbox_inches='tight')#, dpi=100, figsize=(15,15))
 print '\t**Drawing edges ...'
-nx.draw_networkx_edges(graph, pos=graph_layout, with_labels=False, alpha=0.3)
+nx.draw_networkx_edges(graph, edgelist=edgelist, pos=graph_layout, with_labels=False, alpha=0.3)
 print '\t**Saving final pdf ...'
-plt.savefig('correlation_network-ignored_species.pdf', bbox_inches='tight')#, dpi=100, figsize=(15,15))
+plt.savefig('xmantel_correlation_network-%.1f-ignored_species1.pdf' %threshold, bbox_inches='tight')#, dpi=100, figsize=(15,15))
 print '... done!'
 plt.close()
-######################################################
-############### test if gene length influences degree!
-######################################################
-plt.figure()
-plt.hist(group_length, bins=100, alpha=0.7, edgecolor='white')
-plt.tight_layout()
-plt.savefig('hist_length.pdf')
-plt.close()
 
-plt.figure()
-plt.hist(node_colors, bins=100, alpha=0.7, edgecolor='white')
-plt.tight_layout()
-plt.savefig('hist_degree.pdf')
-plt.close()
+for cluster in gene_clusters[:20]:
+    print gene_clusters.index(cluster)
+    print nx.average_clustering(graph.subgraph(cluster))
+    print ''
 
-plt.figure()
-plt.hist(node_sizes, bins=100, alpha=0.7, edgecolor='white')
-plt.tight_layout()
-plt.savefig('hist_num_of_species.pdf')
-plt.close()
 
-plt.figure()
-plt.scatter(group_length, node_colors, alpha=0.3, c='g', edgecolor='none')
-plt.xlim(xmin=0)
-plt.ylim(ymin=0)
-plt.xlabel('Gene family sequence length (MSA length)')
-plt.ylabel('Gene family degree')
-plt.tight_layout()
-plt.savefig('length_VS_degree.pdf')
-plt.close()
 
-plt.figure()
-plt.scatter(node_sizes, node_colors, alpha=0.3, c='g', edgecolor='none')
-plt.xlabel('Num os species where gene family is present')
-plt.ylabel('Gene family degree')
-plt.xlim(xmin=0)
-plt.ylim(ymin=0)
-plt.tight_layout()
-plt.savefig('num_of_sp_VS_degree.pdf')
-plt.close()
-######################################################
 
-######################################################
-############################################ MCL time!
-######################################################
-print '\t**Saving network matrix ...'
-nx.write_weighted_edgelist(graph, 'mcl_input.abc')
 
-print '\t**Writing in MCL-happy format ...'
-system('mcxload -abc mcl_input.abc --stream-mirror -o mcl_input.mci -write-tab mcl_input.tab')
 
-print '\t**Running MCL with different inflation values (-I) ...'
-system('cat inflation_values | xargs -n1 -P10 mcl mcl_input.mci -I ')
-system('clm dist --chain out.mcl_input.mci.I{14,20,25,30,35,40,45,50,55,60,65,70} > clm_dist')
-
-print '\t**Running MCL with selected inflation ...'
-system('mcl mcl_input.mci -I 2 -te 10 -o mcl_output')
-
-system('mcxdump -icl mcl_output -tabr mcl_input.tab -o gene_clusters')
-print '... done!'
-
-gene_clusters = []
-for line in open('gene_clusters').xreadlines():
-    gene_clusters.append(line.split())
 
 for cluster in gene_clusters:
     if len(cluster) < 10:
@@ -322,7 +583,8 @@ hm21_mls = {
 'radA' : '99.2751',
 'zipA' : '99.2310',
 'tsf'  : '99.2388',
-'atpD' : '99.3397'
+'atpD' : '99.3397',
+'metG' : '99.1415'
 }
 mls_groups = {}
 for group in groups:
@@ -334,10 +596,10 @@ for group in groups:
             break
 
 for cluster in gene_clusters:
-    for group in mls_groups.values():
-        if group in cluster:
+    for group in mls_groups:
+        if mls_groups[group] in cluster:
             print group
-            print len(cluster)
+            print gene_clusters.index(cluster)
             print ''
 
 for genes in combinations(mls_groups.keys(), 2):
